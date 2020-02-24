77ec3ff1ae3796283344ed38c54c559aaa5ae136
aashi <aashimittal19@gmail.com>
2020-02-24 13:04:04 -0800

    new stuff in code

---
+++ code/Movies.java
@@ -0,0 +1,33 @@
+package code;
+
+public class Movies extends Product{
+	
+	private String genre; 
+	private double IMDb; 
+	private int MPA; 
+	private String length; 
+	private String release; 
+	
+	
+	public Movies(String myGenre, String myName, String myDescription, double myPrice)
+	{
+		super(myName, myPrice, myDescription);
+	}
+	
+	public String toString()
+	{
+		String temp = ""; 
+		return temp;
+	}
+	
+	public void setDate()
+	{
+		
+	}
+	
+	public void setDescription()
+	{
+		
+	}
+
+}
