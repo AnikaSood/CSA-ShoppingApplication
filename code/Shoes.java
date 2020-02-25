7ce3bdbc5f1af81ef4bba490afa365da8f6ac6e9
ishitav <ishitavas@gmail.com>
2020-02-25 13:40:39 -0800

    Created shoes class

---
+++ code/Shoes.java
@@ -0,0 +1,28 @@
+package code;
+
+public class Shoes extends Product {
+
+	public Shoes(String myName, double myPrice, String myDescription) {
+		super(myName, myPrice, myDescription);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDescription() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
