68091c46806ba5600a72c9e459bc192a7a7b3dbe
aashi <aashimittal19@gmail.com>
2020-02-25 13:38:31 -0800

    edits, have errors

--- code/Movies.java
+++ code/Movies.java
@@ -4,30 +4,37 @@ public class Movies extends Product{
 	
 	private String genre; 
 	private double IMDb; 
-	private int MPA; 
+	private char MPA; 
 	private String length; 
 	private String release; 
+	//private static String price; 
 	
-	
-	public Movies(String myGenre, String myName, String myDescription, double myPrice)
+	public Movies(String myGenre, String myName, String myDescription, double myPrice, double myReview, char myRating, String myLength, String myRelease)
 	{
-		super(myName, myPrice, myDescription);
+		super(myName, myPrice);
+		myGenre = genre; 
+		myReview = IMDb;
+		myRating = MPA; 
+		myLength = length; 
+		myRelease = release; 
+		
+		
 	}
 	
 	public String toString()
 	{
-		String temp = ""; 
-		return temp;
-	}
-	
-	public void setDate()
-	{
+		String name = getName();
+		return name; 
 		
 	}
 	
 	public void setDescription()
 	{
-		
+		String description = price + "\n"; 
 	}
 
+	public static void main(String[] args)
+	{
+		System.out.print(price);
+	}
 }
