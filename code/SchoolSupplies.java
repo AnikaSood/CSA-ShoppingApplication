305535bde1e1e260bff1988830210c78b6ed15de
ishitav <ishitavas@gmail.com>
2020-02-25 14:17:58 -0800

    added school supplies

---
+++ code/SchoolSupplies.java
@@ -0,0 +1,22 @@
+package code;
+
+public class SchoolSupplies extends Product{
+
+	public SchoolSupplies(String myName, double myPrice) {
+		super(myName, myPrice);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDescription() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
