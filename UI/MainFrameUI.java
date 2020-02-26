262e7821fa48f27878517be683160f8cece966f0
Anika Sood <soodanika@gmail.com>
2020-02-26 12:30:19 -0800

    MainFrameUI

--- UI/MainFrameUI.java
+++ UI/MainFrameUI.java
@@ -1,12 +1,21 @@
-package ui;
+package UI;
 
 import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.border.MatteBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.SystemColor;
+import javax.swing.JTextArea;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.ComponentOrientation;
 
 public class MainFrameUI extends JFrame {
 
@@ -41,31 +50,151 @@ public class MainFrameUI extends JFrame {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBackground(Color.WHITE);
-		frame.getContentPane().setBackground(Color.WHITE);
+		frame.getContentPane().setBackground(new Color(220, 220, 220));
 		frame.getContentPane().setLayout(null);
 		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(Color.WHITE);
-		menuBar.setBounds(0, 0, 1370, 57);
 		
 		
+		java.awt.Image image = new ImageIcon("ui/KPLogo.jpg").getImage();
 		
-		java.awt.Image image = new ImageIcon("ui/15332C7B-8ECE-46FD-895D-292C453BBFA8 (1).jpeg").getImage();
-		JLabel logo = new JLabel("ghgfhgfhg");
+		JTextArea txtrSearchBar = new JTextArea();
+		txtrSearchBar.setDragEnabled(false);
+		txtrSearchBar.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		txtrSearchBar.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+		txtrSearchBar.setBounds(308, 17, 778, 16);
+		frame.getContentPane().add(txtrSearchBar);
+		JLabel logo = new JLabel("");
+		logo.setBounds(28, 6, 80, 39);
+		frame.getContentPane().add(logo);
 		
 		
 		logo.setIcon(new ImageIcon(image));
-		//frame.getContentPane().add(logo);
-		frame.getContentPane().add(menuBar);
 		
-		menuBar.add(logo);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(211, 211, 211));
 		
-		//
+		panel.setBounds(147, 6, 144, 39);
+		frame.getContentPane().add(panel);
+		
+		JButton btnCategories = new JButton("   Categories    ");
+		btnCategories.setBorderPainted(false);
+		btnCategories.setBackground(new Color(211, 211, 211));
+		btnCategories.setOpaque(true);
+		
+		JButton btnCategories_1 = new JButton("     Categories    ");
+		btnCategories_1.setOpaque(true);
+		btnCategories_1.setBorderPainted(false);
+		btnCategories_1.setBackground(new Color(211, 211, 211));
+		panel.add(btnCategories_1);
+		
+		JButton catBooks = new JButton("        Books       ");
+		
+		catBooks.setOpaque(true);
+		catBooks.setBorderPainted(false);
+		catBooks.setBackground(Color.LIGHT_GRAY);
+		panel.add(catBooks);
+		
+		JButton catMovies = new JButton("        Movies      ");
+		catMovies.setOpaque(true);
+		catMovies.setBorderPainted(false);
+		catMovies.setBackground(Color.LIGHT_GRAY);
+		panel.add(catMovies);
+		
+		JButton catSchool = new JButton("School Supplies");
+		catSchool.setOpaque(true);
+		catSchool.setBorderPainted(false);
+		catSchool.setBackground(Color.LIGHT_GRAY);
+		panel.add(catSchool);
+		
+		JButton catShoes = new JButton("       Shoes        ");
+		catShoes.setOpaque(true);
+		catShoes.setBorderPainted(false);
+		catShoes.setBackground(Color.LIGHT_GRAY);
+		panel.add(catShoes);
+		
+		JLabel label = new JLabel("            ");
+		label.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(169, 169, 169)));
+		label.setBounds(259, 6, 828, 39);
+		frame.getContentPane().add(label);
+		label.setBackground(Color.WHITE);
+		label.setOpaque(true);
+		
+		
+		
+		JLabel label_1 = new JLabel("            ");
+		label_1.setOpaque(true);
+		label_1.setBackground(new Color(255, 255, 255));
+		label_1.setBounds(0, 0, 1370, 50);
+		frame.getContentPane().add(label_1);
 		frame.setBounds(0, 0, 1370, 775);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//CATEGORIES HOVER
+		btnCategories_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
 		
+		//BOOKS HOVER
+		catBooks.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
+		catMovies.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
+		catShoes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
+		catSchool.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 175);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panel.setBounds(147, 6, 144, 39);
+			}
+		});
 		
 		
 	}
 }
+
+
